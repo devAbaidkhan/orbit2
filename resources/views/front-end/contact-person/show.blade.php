6b74d79d3f98c776d74e79be99f248b46c6447e9
hamza.azmat <hamzaazmat@ktechz.com>
2022-02-25 21:03:18 +0500

    show
    shoe

--- resources/views/front-end/contact-person/show.blade.php
+++ resources/views/front-end/contact-person/show.blade.php
@@ -75,19 +75,6 @@ job-grid -->
                         </div>
                     @endforeach
                 </div>
-                <div class="row">
-                    <div class="col-12 text-center mt-4">
-                        <ul class="pagination justify-content-center mb-0">
-                            <li class="page-item disabled"> <span class="page-link b-radius-none">Prev</span> </li>
-                            <li class="page-item active" aria-current="page"><span class="page-link">1 </span> <span class="sr-only">(current)</span></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item"><a class="page-link" href="#">...</a></li>
-                            <li class="page-item"><a class="page-link" href="#">25</a></li>
-                            <li class="page-item"> <a class="page-link" href="#">Next</a> </li>
-                        </ul>
-                    </div>
-                </div>
             </div>
         </div>
         </div>
